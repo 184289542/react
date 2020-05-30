95e0abcbfcc7ebdd1257adc3c52b121d493de1e8
184289542 <184289542@qq.com>
2020-05-31 04:39:39 +0800

    小白

---
+++ src/pages/loginregister/Loginregister.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Login extends React.Component{
+    render() {
+        return (
+            <div>
+                <p>京东登陆注册</p>
+            </div>
+        )
+    }
+}
+export default Login
--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,8 @@
 import React from "react";
 import {HashRouter as Router, Route} from "react-router-dom";
-
+import loginregister from "../pages/loginregister/Loginregister"
+import {Link} from "react-router-dom";
+// import home from "../pages/loginregister/Home"
 
 class MyRouter extends React.Component {
     render() {
@@ -8,7 +10,8 @@ class MyRouter extends React.Component {
             // 路由容器：所有的Route与Link这些组件都要放置在内部
             <Router> 
                 <div>
-                    // <Route path="/" exact component={IndexPage}></Route>
+                    {/* <Route path="/login" component={loginregister}></Route> */}
+                    {/* <Route path="/" component={home}></Route> */}
                 </div>
             </Router>
         )
